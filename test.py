73ad6670cbb2ce0ec785989feeae391b522df81e
JoInCp <kimdang185@gmail.com>
2023-08-17 16:27:53 +0900

    2023-08-17_HP가 0이 될시 다시하기 버튼 추가

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pygame
+import copy
 
 # ========== 초기 설정 ==========
 pygame.init()
@@ -13,14 +14,16 @@ black = (0, 0, 0)
 red = (255, 0, 0)
 
 # ========== 폰트 설정 ==========
+path = "KBO Dia Gothic_medium.ttf"
+
 button_font_size = 50
-button_font = pygame.font.Font(None, button_font_size)
+button_font = pygame.font.Font(path, button_font_size)
 hp_label_font = pygame.font.Font(None, 40)
 hp_label_text = hp_label_font.render("MY.HP", True, black)
-hp_label_rect = hp_label_text.get_rect(right=screen_width-230, top=150)
-comb_font = pygame.font.Font(None, 40)
-comb_number_font_size = 100
-comb_number_font = pygame.font.Font(None, comb_number_font_size)
+hp_label_rect = hp_label_text.get_rect(right=screen_width-240, top=160)
+comb_font = pygame.font.Font(path, 40)
+comb_number_font_size = 80
+comb_number_font = pygame.font.Font(path, comb_number_font_size)
 
 # ========== 게임 상태 변수 ==========
 comb = 0
@@ -37,13 +40,15 @@ note_height = 40
 note_width = 80
 semicolon_key = 59
 hp_bar_x = screen_width - 320 
-hp_bar_y = hp_label_rect.bottom + 10
+hp_bar_y = hp_label_rect.bottom 
 hp_bar_width = 240
 hp_bar_height = 60
 hp_segment_width = hp_bar_width // 10 
 player_hp = 10
 button_x_centers = [73.75 + 45, 184.75 + 45, 296 + 45, 407 + 45]
 button_y_center = rect_height - 132 + 65
+check_line = rect_y + rect_height - 150
+hit_line = rect_y + rect_height - 240
 
 # ========== 버튼 데이터 ==========
 button_data = [
@@ -54,32 +59,38 @@ button_data = [
 ]
 
 # ========== 노트 데이터 ==========
-note_data = [
-    {"number": 0, "color": black, "lane": 0, "note_start_delays": 0,"note_speed": 1/10},
-    {"number": 1, "color": black, "lane": 1, "note_start_delays": 700,"note_speed": 1/10},
-    {"number": 2, "color": black, "lane": 2, "note_start_delays": 700,"note_speed": 1/10},
-    {"number": 3, "color": black, "lane": 3, "note_start_delays": 1500,"note_speed": 1/10},
-    {"number": 4, "color": black, "lane": 1, "note_start_delays": 1800,"note_speed": 1/10},
-    {"number": 5, "color": black, "lane": 2, "note_start_delays": 1800,"note_speed": 1/10},
-    {"number": 6, "color": black, "lane": 3, "note_start_delays": 2300,"note_speed": 1/10}, 
-    {"number": 7, "color": black, "lane": 1, "note_start_delays": 2300,"note_speed": 1/10},
-    {"number": 8, "color": black, "lane": 0, "note_start_delays": 2700,"note_speed": 1/10},
-    {"number": 9, "color": black, "lane": 1, "note_start_delays": 3300,"note_speed": 1/10},
-    {"number": 10, "color": black, "lane": 2, "note_start_delays": 4000,"note_speed": 1/10},
-    {"number": 11, "color": black, "lane": 3, "note_start_delays": 4000,"note_speed": 1/10},
+original_note_data = [
+    {"number": 0, "color": black, "lane": 0, "note_start_delays": 0,"note_speed": 1/15},
+    {"number": 1, "color": black, "lane": 1, "note_start_delays": 1000,"note_speed": 1/15},
+    {"number": 2, "color": black, "lane": 2, "note_start_delays": 1000,"note_speed": 1/15},
+    {"number": 3, "color": black, "lane": 3, "note_start_delays": 1800,"note_speed": 1/15},
+    {"number": 4, "color": black, "lane": 1, "note_start_delays": 2500,"note_speed": 1/15},
+    {"number": 5, "color": black, "lane": 2, "note_start_delays": 2500,"note_speed": 1/15},
+    {"number": 6, "color": black, "lane": 3, "note_start_delays": 3200,"note_speed": 1/15}, 
+    {"number": 7, "color": black, "lane": 1, "note_start_delays": 3200,"note_speed": 1/15},
+    {"number": 8, "color": black, "lane": 0, "note_start_delays": 4000,"note_speed": 1/15},
+    {"number": 9, "color": black, "lane": 1, "note_start_delays": 4500,"note_speed": 1/15},
+    {"number": 10, "color": black, "lane": 2, "note_start_delays": 4500,"note_speed": 1/15},
+    {"number": 11, "color": black, "lane": 3, "note_start_delays": 5000,"note_speed": 1/15},
 ]
 
 initial_position_outside_screen = -5  # 더 큰 값을 사용해도 됩니다.
-for note in note_data:
+for note in original_note_data:
     note["start_time"] = pygame.time.get_ticks() + note["note_start_delays"]
     note["number"] = initial_position_outside_screen
 
 retry_button = {
-    'color': black,
+    'color': white,
     'rect': pygame.Rect(screen_width // 2 - 100, screen_height // 2 + 50, 200, 50),
     'text': "다시 시도하기",
-    'font': pygame.font.Font(None, 30)
+    'font': pygame.font.Font(path, 30),
+    'text_color': black 
 }
+
+note_data = copy.deepcopy(original_note_data)
+for note in note_data:
+    note["hit"] = False
+
 
 # ========== 함수 정의 ==========
 
@@ -107,20 +118,21 @@ clock = pygame.time.Clock()
 def move_notes():
     global comb, y_position, bottom_of_screen, player_hp, game_state  
     bottom_of_screen = rect_y + rect_height  
-    hit_line = rect_y + rect_height - 240
     current_time = pygame.time.get_ticks()
     notes_to_remove = []
-
+    
     for note in note_data:
         if current_time >= note["start_time"]:
             note["number"] += note["note_speed"]
             y_position = note["number"] * note_height
 
-            if hit_line <= y_position and y_position <= hit_line + note_height:
-                player_hp -= 1 
-                if note not in notes_to_remove:
-                    notes_to_remove.append(note)
-                comb = 0
+            if y_position + note_height > check_line:
+                if not note["hit"]: 
+                    note["hit"] = True 
+                    player_hp -= 1 
+                    comb = 0
+                    if note not in notes_to_remove:
+                        notes_to_remove.append(note)
 
             elif y_position >= bottom_of_screen:
                 if note not in notes_to_remove:
@@ -129,17 +141,21 @@ def move_notes():
     for note in notes_to_remove:
         note_data.remove(note)
 
-
 def check_game_over(): # 게임 오버 체크
     global game_state, player_hp
     if player_hp <= 0:
         game_state = "game_over"
 
+def draw_retry_button():
+    pygame.draw.rect(screen, retry_button['color'], retry_button['rect'])
+    text = retry_button['font'].render(retry_button['text'], True, retry_button['text_color'])
+    text_rect = text.get_rect(center=retry_button['rect'].center)
+    screen.blit(text, text_rect)
+
+
 # ========== 메인 게임 루프 ==========
 while running:
     screen.fill(white) 
-
-    move_notes()  
     draw_hp_bar()
     
     for event in pygame.event.get():
@@ -154,12 +170,27 @@ while running:
                     hit_note = False
                     for note in note_data:
                         y_position = note["number"] * note_height
-                        line_y = rect_y + rect_height - 240
+                        line_y = hit_line
                         if note["lane"] == lane and y_position <= line_y and y_position + note_height >= line_y:
                             hit_note = True
                             break
                     if not hit_note:
                         comb = 0
+        if game_state == "game_over":
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if retry_button['rect'].collidepoint(mouse_pos):
+                    comb = 0
+                    player_hp = 10
+                    game_state = "playing"
+                    
+                    note_data = copy.deepcopy(original_note_data)
+                    
+                    for note in note_data:
+                        note["start_time"] = pygame.time.get_ticks() + note["note_start_delays"]
+                        note["number"] = initial_position_outside_screen
+                        note["hit"] = False
+
 
         elif event.type == pygame.KEYUP:
             for button in button_data:
@@ -168,6 +199,7 @@ while running:
     
     # 게임 중일 때
     if game_state == "playing":
+        screen.blit(hp_label_text, hp_label_rect.topleft)
         move_notes()
         check_game_over()
         pygame.draw.rect(screen, black, (rect_x, rect_y, rect_width, rect_height), border_thickness)
@@ -177,19 +209,17 @@ while running:
             x = rect_x + i * segment_width
             pygame.draw.line(screen, black, (x, rect_y), (x, rect_y + rect_height-5), 8)
 
-        y = rect_y + rect_height - 150
-        pygame.draw.line(screen, black, (rect_x, max(y, rect_y)), (rect_x + rect_width-5, max(y, rect_y)), 8)
+        pygame.draw.line(screen, black, (rect_x, max(check_line, rect_y)), (rect_x + rect_width-5, max(check_line, rect_y)), 8)
 
-        y = rect_y + rect_height - 240
-        pygame.draw.line(screen, black, (rect_x, max(y, rect_y)), (rect_x + rect_width-5, max(y, rect_y)), 8)
+        pygame.draw.line(screen, black, (rect_x, max(hit_line, rect_y)), (rect_x + rect_width-5, max(hit_line, rect_y)), 8)
 
-        for button in button_data:
+        for button in button_data:  
             if button["color"] == white:  
                 for note in note_data[:]:
                     if note["number"] >= 0:
                         lane = note["lane"]
                         y_position = note["number"] * note_height
-                        line_y = rect_y + rect_height - 240
+                        line_y = hit_line
                         if y_position <= line_y and y_position + note_height >= line_y:
                             if lane == button_data.index(button):
                                 comb += 1
@@ -210,10 +240,10 @@ while running:
             text_rect = text.get_rect(center=(button["x"] + button["width"] // 2, button["y"] + button["height"] // 2))
             screen.blit(text, text_rect)
 
-        comb_text = comb_font.render("COMB", True, black)
-        comb_rect = comb_text.get_rect(right=screen_width-160, top=400)
-        pygame.draw.circle(screen, black, comb_rect.center, comb_rect.width, 8)
-        comb_text_rect = comb_text.get_rect(center=(comb_rect.centerx, comb_rect.centery - 100))
+        comb_text = comb_font.render("COMB", True, black)   
+        comb_rect = comb_text.get_rect(right=screen_width-140, top=450)
+        pygame.draw.circle(screen, black, comb_rect.center, comb_rect.width-30, 8)
+        comb_text_rect = comb_text.get_rect(center=(comb_rect.centerx, comb_rect.centery - 130))
         screen.blit(comb_text, comb_text_rect)
         comb_number_text = comb_number_font.render(str(comb), True, black)
         comb_number_rect = comb_number_text.get_rect(center=comb_rect.center)
@@ -225,10 +255,12 @@ while running:
     # 게임 오버일 때
     elif game_state == "game_over":
         screen.fill(white)
-        game_over_font = pygame.font.Font(None, 100)
+        game_over_font = pygame.font.Font(path, 100)
         game_over_text = game_over_font.render("Game Over", True, black)
         text_rect = game_over_text.get_rect(center=(screen_width // 2, screen_height // 2))
+        draw_retry_button() 
         screen.blit(game_over_text, text_rect)
         pygame.display.flip()
+        
         
 pygame.quit()
